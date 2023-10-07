81196d44161b782456ae8ecfd856d04a1e3c2637
Damian <swean@vp.pl>
2023-10-08 00:38:11 +0200

    j

--- netlify/functions/fetchApi/fetchApi.js
+++ netlify/functions/fetchApi/fetchApi.js
@@ -8,6 +8,122 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration)
 
+
+//------------------------Eleven----------
+
+function elevenSpeak(message){
+
+  let audioDataArray = []
+const AudioContext = window.AudioContext || window.webkitAudioContext
+const audioContext = new AudioContext();
+let nextTime = 0; // This variable will keep track of the time the next chunk should start
+
+  const voiceId = "XrExE9yKIg1WjnnlVkGX"; // replace with your voice_id
+      const model = 'eleven_monolingual_v1';
+      const wsUrl = `wss://api.elevenlabs.io/v1/text-to-speech/${voiceId}/stream-input?model_id=${model}&optimize_streaming_latency=4&output_format=mp3_44100`;
+      const socket = new WebSocket(wsUrl);
+     
+  // 2. Initialize the connection by sending the BOS message
+  socket.onopen = function (event) {
+
+      const bosMessage = {
+          "text": " ",
+          "voice_settings": {
+              "stability": 0.5,
+              "similarity_boost": true
+          },
+          "xi_api_key": 'eleven', // replace with your API key --------------------------------------- to change
+      };
+  
+      socket.send(JSON.stringify(bosMessage));
+  
+      // 3. Send the input text message ("Hello World")
+      
+     const textMessage = {
+          "text": ` ${message},,,,, `,
+          "try_trigger_generation": true,
+      };
+     socket.send(JSON.stringify(textMessage)) ;
+
+      // 4. Send the EOS message with an empty string
+      const eosMessage = {
+          "text": ""
+      };
+  
+      socket.send(JSON.stringify(eosMessage));
+  };
+  
+  // 5. Handle server responses
+  socket.onmessage = async function (event) {
+      const response = await JSON.parse(event.data);
+
+      console.log("Server response:", response);
+  
+      if (response.audio) {
+          // decode and handle the audio data (e.g., play it)
+    // convert base64 to Uint8Array
+  
+    const data = Uint8Array.from(atob(response.audio), c => c.charCodeAt(0));
+            
+
+      // decode the audio data asynchronously 
+      const buffer = await audioContext.decodeAudioData(data.buffer).then(decodedAudio => { audioDataArray.push(decodedAudio)});
+
+      
+let source
+      audioDataArray.forEach( decodedAudio => { 
+           source = audioContext.createBufferSource() 
+          source.buffer = decodedAudio
+           source.connect(audioContext.destination)
+          
+          })
+   
+      // Start the source to play the audio
+      if (nextTime == 0) {
+          nextTime = audioContext.currentTime;
+          }
+          for(let i = 0; i < audioDataArray.length; i++)
+         { 
+          source.start(nextTime)
+          nextTime += source.buffer.duration
+      }
+      
+ 
+//----------------------------------------------------------------------------
+    
+          console.log("Received audio chunk");
+      } else {
+          console.log("No audio data in the response");
+      }
+  
+      if (response.isFinal) {
+          // the generation is complete
+         
+      }
+
+      if (response.normalizedAlignment) {
+          // use the alignment info if needed
+  
+      }
+
+  // Handle errors
+  socket.onerror = function (error) {
+      console.error(`WebSocket Error: ${error}`);
+  };
+  
+  // Handle socket closing
+  socket.onclose = function (event) {
+      if (event.wasClean) {
+          console.info(`Connection closed cleanly, code=${event.code}, reason=${event.reason}`);
+      } else {
+          console.warn('Connection died');
+      }
+  };
+
+  }
+
+}
+
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
   try {
