4c3684793461b366d1448bb77c3e997a3e5f00eb
Damian <swean@vp.pl>
2023-10-07 23:44:19 +0200

    Update fetchApi.js

--- netlify/functions/fetchApi/fetchApi.js
+++ netlify/functions/fetchApi/fetchApi.js
@@ -11,17 +11,21 @@ const openai = new OpenAIApi(configuration)
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
   try {
-    const response = await openai.createChatCompletion({ //-------------------competion
-      model: 'gpt-3.5-turbo',
-      messages: event.body,
-      presence_penalty: 0,
-      frequency_penalty: 0.3
-  })
-    const subject = event.queryStringParameters.name || 'World'
+  //   const response = await openai.createChatCompletion({ //-------------------competion
+  //     model: 'gpt-3.5-turbo',
+  //     messages: event.body,
+  //     presence_penalty: 0,
+  //     frequency_penalty: 0.3
+  // })
+
+  const response = {"test"}
+   
     return {
       statusCode: 200,
-      body: JSON.stringify({
-        reply:response.data }),
+      body: JSON.stringify(
+        {
+        reply:response.data }
+        ),
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
