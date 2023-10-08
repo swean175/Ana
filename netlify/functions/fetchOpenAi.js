6ac3e76896e2ed7e57669709242a3c6f137a756a
Damian <swean@vp.pl>
2023-10-08 04:26:03 +0200

    Update fetchOpenAi.js

--- netlify/functions/fetchOpenAi.js
+++ netlify/functions/fetchOpenAi.js
@@ -17,15 +17,15 @@ const openai = new OpenAIApi(configuration)
 const handler = async (event) => {
   try {
 
-    // const requestBody = JSON.parse(event.body); // Parse the request body as JSON
-    // const messages = requestBody.messages.map((message) => ({
-    //   role: message.role,
-    //   content: message.content,
-    // }));
+    const requestBody = JSON.parse(event.body); // Parse the request body as JSON
+    const messages = requestBody.messages.map((message) => ({
+      role: message.role,
+      content: message.content,
+    }));
 
     const response = await openai.createChatCompletion({ 
       model: 'gpt-3.5-turbo',
-      messages: JSON.parse(event.body),
+      messages: messages,
       presence_penalty: 0,
       frequency_penalty: 0.3
  })
@@ -34,7 +34,6 @@ const handler = async (event) => {
       statusCode: 200,
       body: JSON.stringify({
         reply:response.data
-      
       }),
    
     }
