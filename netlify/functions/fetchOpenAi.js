5f990f20147f39564e0b60d2d2ce06e649d20077
Damian <swean@vp.pl>
2023-10-10 00:41:57 +0200

    Update fetchOpenAi.js

--- netlify/functions/fetchOpenAi.js
+++ netlify/functions/fetchOpenAi.js
@@ -1,6 +1,7 @@
 import { Configuration, OpenAIApi } from 'openai'
 
-const {Configuration, OpenAiApi} = require(openai)
+// const {Configuration, OpenAiApi} = require(openai)
+
 //-------------------------------
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY, 
