9dda9bb2751abcf42c5454408e3132f10ea1062e
Damian <swean@vp.pl>
2023-10-10 01:49:24 +0200

    Update test.js

--- netlify/functions/fetchOpenAi/test.js
+++ netlify/functions/fetchOpenAi/test.js
@@ -1,22 +1,9 @@
-import { Configuration, OpenAIApi } from 'openai'
 
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-})
 
-const openai = new OpenAIApi(configuration)
 
 const handler = async (event) => {
     try {
-        const response = await openai.createCompletion({
-            model: 'davinci:ft-scrimba-2023-03-30-23-10-03',
-            prompt: event.body,
-            max_tokens: 500,
-            presence_penalty: 0.3,
-            frequency_penalty: 0,
-            temperature: 0,
-            stop: ['\n', '->']
-        })
+        const response = "test working"
         return {
             statusCode: 200,
             body: JSON.stringify({
