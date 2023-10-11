208bc99910d86144c1094ca031e6827066c38cf5
Damian <swean@vp.pl>
2023-10-12 00:32:36 +0200

    Update fetchEleven.js

--- netlify/functions/fetchEleven.js
+++ netlify/functions/fetchEleven.js
@@ -3,12 +3,12 @@
   const sdk = require('api')('@genny-api/v1.0#eqij1clm8s4s2v');
 
 exports.handler = async function (event, context) {
-    try {
+
 
 
         sdk.auth(process.env.GENNY_API_KEY);
       
-       const response = await sdk.syncTts({speed: 1, text: "Jestem Ana", speaker: '640f477d2babeb0024be422b'})
+    const response = await sdk.syncTts({speed: 1, text: "Jestem Ana", speaker: '640f477d2babeb0024be422b'})
         //   .then(({ data }) => console.log(data))
           .then(({data}) => data)
           .catch(err => console.error(err));
@@ -18,9 +18,7 @@ exports.handler = async function (event, context) {
         body: JSON.stringify({"reply":response}),
     }
 
-  } catch (error) {
-    return { statusCode: 500, body: error.toString("dont know") }
-  }
+
 };
 
 
