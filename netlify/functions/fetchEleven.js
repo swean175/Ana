06826271e6cbe4bc0d8747cd44569d041e3b453e
Damian <swean@vp.pl>
2023-10-12 00:25:41 +0200

    Update fetchEleven.js

--- netlify/functions/fetchEleven.js
+++ netlify/functions/fetchEleven.js
@@ -9,12 +9,13 @@ exports.handler = async function (event, context) {
         sdk.auth(process.env.GENNY_API_KEY);
       
        const response = await sdk.syncTts({speed: 1, text: "Jestem Ana", speaker: '640f477d2babeb0024be422b'})
-          .then(({ data }) => console.log(data))
+        //   .then(({ data }) => console.log(data))
+          .then(({data}) => data)
           .catch(err => console.error(err));
     
     return {
         statusCode: 200,
-        body: JSON.stringify({"reply":response.data[0]}),
+        body: JSON.stringify({"reply":response}),
     }
 
   } catch (error) {
