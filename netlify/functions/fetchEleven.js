0e670d8702c67c5dc8a29309634ca9fcc6b3aaa0
Damian <swean@vp.pl>
2023-10-12 00:41:20 +0200

    Update fetchEleven.js

--- netlify/functions/fetchEleven.js
+++ netlify/functions/fetchEleven.js
@@ -1,21 +1,42 @@
 
 
-  const sdk = require('api')('@genny-api/v1.0#eqij1clm8s4s2v');
+//   const sdk = require('api')('@genny-api/v1.0#eqij1clm8s4s2v');
 
 exports.handler = async function (event, context) {
 
-        sdk.auth(process.env.GENNY_API_KEY);
-      
-    // const response = await 
-    sdk.syncTts({speed: 1, text: "Jestem Ana", speaker: '640f477d2babeb0024be422b'})
-          .then(({ data }) => console.log(data))
-        //   .then(({data}) => data)
-          .catch(err => console.error(err));
+   
+    try {
+        const options = {
+            method: 'POST',
+            headers: {
+              accept: 'application/json',
+              'content-type': 'application/json',
+              'X-API-KEY': process.env.GENNY_API_KEY
+            },
+            body: JSON.stringify({speed: 1, text: 'Jestem Ana', speaker: '640f477d2babeb0024be422b'})
+          };
+
+        const response = await fetch('https://api.genny.lovo.ai/api/v1/tts/sync', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+     
+        return {
+        
+          statusCode: 200,
+          body: JSON.stringify({"reply":response}),
+       
+        }
     
-    // return {
-    //     statusCode: 200,
-    //     body: JSON.stringify({"reply":response}),
-    // }
+      } catch (error) {
+        return { statusCode: 500, body: error.toString("dont know") }
+      }
+    }
+
+
+   
+      
+    
 
 
 };
