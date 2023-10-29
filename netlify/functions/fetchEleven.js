43b107a804d98b49b846e301a100ac1cbebe4278
Damian Szudra <123982916+swean175@users.noreply.github.com>
2023-10-29 23:43:09 +0100

    Update fetchEleven.js

--- netlify/functions/fetchEleven.js
+++ netlify/functions/fetchEleven.js
@@ -1,27 +1,6 @@
 
 
-  const sdk = require('api')('@genny-api/v1.0#eqij1clm8s4s2v');
 
-exports.handler = async function (event, context) { 
-    try {
-       
-        sdk.auth( process.env.GENNY_API_KEY);
-        const res = await sdk.syncTts({speed: 1, text: 'I am Ana Your faithfull assisatant ', speaker: '640f477d2babeb0024be422b'})
-        .then(({data}) => console.log(data))
-        .catch(err => console.error(err));
-
-       
-        return {
-        
-          statusCode: 200,
-          body: {"reply":res}
-       
-        }
-    
-      } catch (error) {
-        return { statusCode: 500, body: error.toString("dont know") }
-      }
-    }
 
 
 
