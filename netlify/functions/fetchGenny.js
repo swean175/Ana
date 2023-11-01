1a62daa0fe52cea6ab3fe846f3b7b4e8f5910a87
Damian <swean@vp.pl>
2023-11-01 21:33:06 +0100

    Update fetchGenny.js

--- netlify/functions/fetchGenny.js
+++ netlify/functions/fetchGenny.js
@@ -1,13 +1,11 @@
 
 
-
-
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
     const options = {
   method: 'POST',
   headers: {
-    accept: 'text/event-stream',
+    accept: 'audio/mpeg',
     'content-type': 'application/json',
     AUTHORIZATION: process.env.GENNY_API_KEY,
     'X-USER-ID': 'fpHJRFyzBxVVNwDVuTkh6FrHhyx1'
@@ -24,9 +22,7 @@ const handler = async (event) => {
 
   const res = fetch('https://api.play.ht/api/v2/tts/stream', options)
   .then(response => response.json())
-  .catch(err => console.error(err));
-    
-
+  .catch(err => console.error(err))
     
     return {
     
