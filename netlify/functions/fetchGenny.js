2335f4d16b48b7c4b48ffd87f2b0add7793bb0d6
Damian <swean@vp.pl>
2023-11-02 03:11:32 +0100

    Update fetchGenny.js

--- netlify/functions/fetchGenny.js
+++ netlify/functions/fetchGenny.js
@@ -20,8 +20,19 @@ const handler = async (event) => {
 
 
 const options = {
-  method: 'GET',
-  headers: {accept: 'application/json', AUTHORIZATION: process.env.GENNY_API_KEY, 'X-USER-ID': 'fpHJRFyzBxVVNwDVuTkh6FrHhyx1'}
+  method: 'POST',
+  headers: {
+    accept: 'text/event-stream',
+    'content-type': 'application/json',
+    AUTHORIZATION: process.env.GENNY_API_KEY,
+    'X-USER-ID': 'fpHJRFyzBxVVNwDVuTkh6FrHhyx1'
+  },
+  body: JSON.stringify({
+    text: 'Hello from a realistic voice.',
+    voice: 's3://voice-cloning-zero-shot/d9ff78ba-d016-47f6-b0ef-dd630f59414e/female-cs/manifest.json',
+    output_format: 'mp3',
+    voice_engine: 'PlayHT2.0'
+  })
 };
 
 try {
