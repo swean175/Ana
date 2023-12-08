a59c134459e2153ea99eb63d3e20afd4fef20d94
Damian <swean@vp.pl>
2023-12-09 00:11:41 +0100

    d

--- index.js
+++ index.js
@@ -205,9 +205,6 @@ function renderTypewriterText(text) {
         i++
         chatbotConversation.scrollTop = chatbotConversation.scrollHeight
     }, 50)
-    chatbotConversation.appendChild(aud)
-    aud.innerHTML=`<audio id="audioPlayer" controls>
-    </audio>`
 }
 
 document.getElementById('clear-btn').addEventListener('click', () => {
