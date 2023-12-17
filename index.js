4b2fbd17ad41a8c0b9eac307a33eb2f95a239f90
Damian Szudra <123982916+swean175@users.noreply.github.com>
2023-12-17 02:05:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -129,7 +129,7 @@ async function fetchGenny(message){
       fetch(`https://06rdqx.buildship.run/voice?text=${message}`) 
         // .then(response => response.json())
         .then(response => console.log(response))
-        .then(response => say(response.data))
+        .then(response => say(response.response))
         .catch(err => console.error(err));
        //-------------------------------------------------------------
         }
