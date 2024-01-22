83293c45733c35bcee5265e5c06e8ac20634f3ab
Damian Szudra <123982916+swean175@users.noreply.github.com>
2024-01-22 18:10:42 +0100

    Update fetchGenny.js

--- netlify/functions/fetchGenny.js
+++ netlify/functions/fetchGenny.js
@@ -1,59 +1,21 @@
+const handler = async () => {
+  try {
 
 
-
-const handler = async (event) => {
-
-
-const options = {
-  method: 'POST',
-  headers: {
-    accept: 'text/event-stream',
-    'content-type': 'application/json',
-    AUTHORIZATION: process.env.GENNY_API_KEY,
-    'X-USER-ID': 'fpHJRFyzBxVVNwDVuTkh6FrHhyx1'
-  },
-  body: JSON.stringify({
-    text: 'Hello from a realistic voice.',
-    voice: 's3://voice-cloning-zero-shot/d9ff78ba-d016-47f6-b0ef-dd630f59414e/female-cs/manifest.json',
-    output_format: 'mp3',
-    voice_engine: 'PlayHT2.0'
-  })
-};
-
-try {
-
-  // const response = await fetch('https://api.play.ht/api/v2/tts', options)
-  //     if (!response.ok) {
-  //         throw new Error('Network response was not ok');
-  //     }
-  //     const contentType = response.headers.get("Content-Type");
-  //     console.log(contentType);
-  //     const data = await response;
-      // Do something with the data
+    const api = process.env.BUID_API
     
 
-
- 
-   res = fetch('https://api.play.ht/api/v2/tts', options)
-  .then(response => response.json())
-  .then(response =>  console.log("respone"+response))
-  .catch(err => console.error(err))
-    
- 
     return {
     
       statusCode: 200,
-      body: JSON.stringify({"reply":res.content}),  //response.data.url
+      body: JSON.stringify({res:api}),
    
     }
 
   } catch (error) {
     return { statusCode: 500, body: error.toString("dont know") }
   }
-  }
-
-
- 
+}
 
 module.exports = { handler }
 
