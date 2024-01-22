bf1ecb459f78696d7bea7c5af121f6ce94700ed9
Damian Szudra <123982916+swean175@users.noreply.github.com>
2024-01-22 17:28:33 +0100

    Update fetchUrl-background.js

--- netlify/functions/fetchUrl-background.js
+++ netlify/functions/fetchUrl-background.js
@@ -1,1 +1,21 @@
 
+const handler = async () => {
+  try {
+
+
+    const res = {jajo:"jajo-test"}
+
+
+    return {
+    
+      statusCode: 200,
+      body: JSON.stringify({res}),
+   
+    }
+
+  } catch (error) {
+    return { statusCode: 500, body: error.toString("dont know") }
+  }
+}
+
+module.exports = { handler }
